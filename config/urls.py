9b5829f4feaac69509c252eaafeaf37f395902dc
vcarpentier <vjpcarpentier@gmail.com>
2025-10-23 15:57:24 +0100

    Basic httpresponse

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sprint.views import my_sprint
 
 urlpatterns = [
+    path('', my_sprint, name='my_sprint'),
     path('admin/', admin.site.urls),
 ]
