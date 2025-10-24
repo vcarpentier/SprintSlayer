1a516fe57e0c52cd589d8d6516965b074e344425
vcarpentier <vjpcarpentier@gmail.com>
2025-10-24 10:14:36 +0100

    Added Task model

--- sprint/models.py
+++ sprint/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+TASKTYPE = ((0, 'Feature'), (1, 'Bug'), (2, 'refactor'))
+STATUS = ((0, 'To Do'), (1, 'In Progress'), (2, 'Done'))
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_tasks', null=True, blank=True)
+    type = models.IntegerField(choices=TASKTYPE, default=0)
+
+    def __str__(self):
+        return self.title
