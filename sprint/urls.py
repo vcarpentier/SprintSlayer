597beab6e009b861c3c726199ad09d4404be2fcd
vcarpentier <vjpcarpentier@gmail.com>
2025-10-27 16:52:39 +0000

    Started work on the Task list layout.

--- sprint/urls.py
+++ sprint/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TaskList
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='TaskList'),  
+    path('', TaskList.as_view(), name='home'),  
     #path('<title:title>/', views.task_detail, name='task_detail'),
 ]
