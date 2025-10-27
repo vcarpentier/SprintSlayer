a7931758fa8d89be4b71f882b8c3faa04cac9f72
vcarpentier <vjpcarpentier@gmail.com>
2025-10-27 14:27:00 +0000

    Added static and css

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Quick-start development settings - unsuitable for production
